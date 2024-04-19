054cd151a9ebd56cb0c53fe88f3e5a3e12c2dafb
ivana spinoso <ivanaspinoso@MacBook-Air-de-Agustin.local>
2024-04-19 18:41:11 -0300

    footer y detalle

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,37 +2,48 @@ import React from "react";
 import ItemsContainer from "./ItemsContainer";
 import SocialIcons from "./SocialIcons";
 import { Icons } from "./Menus";
+import logo from "./../../public/logo.jpeg"
 
 const Footer = () => {
     return (
-      <footer className="bg-white text-black mt-auto">
-        {/* Utilizamos flexbox para alinear el contenido verticalmente */}
-        <div className="flex justify-between items-center px-4 py-7 md:px-12 bg-[#ffffff19]">
-          <h1 className="text-3xl md:text-4xl font-semibold leading-normal lg:w-2/5 mb-6">
-            <span className="text-pink-400">¡Destaca</span> tu estilo con nuestra moda femenina!
-          </h1>
-          <div>
-            <input
-              type="text"
-              placeholder="Ingresa tu correo electrónico"
-              className="w-full sm:w-72 px-2 py-2.5 text-gray-800 rounded focus:outline-none"
-            />
-            <button className="bg-pink-400 hover:bg-pink-600 duration-300 px-5 py-2.5 font-[Poppins] rounded-md text-white md:w-auto w-full mt-4">
-              Suscribirse
-            </button>
+         <footer className="bg-white text-black mt-auto border-t-4  ">        {/* Utilizamos flexbox para alinear el contenido verticalmente */}
+     <div className="flex flex-row justify-center items-center px-4 py-7 md:px-12 bg-[#ffffff19]">
+    <div className="flex flex-col items-center">
+        <h1 className="text-3xl md:text-4xl font-semibold leading-normal lg:w-5/5 mb-6 relative text-pink-500">
+            ¡Suscribite a nuestro NewsLetter para recibir novedades!
+        </h1>
+    </div>
+
+    <div className="flex flex-row items-center justify-center ml-2">
+        <input
+            type="text"
+            placeholder="Ingresa tu correo electrónico"
+            className="w-full sm:w-72 px-2 py-2.5 text-gray-800 rounded border border-pink-400 focus:outline-none"
+        />
+        <button className="bg-pink-500 hover:bg-pink-600 duration-300 px-5 py-2.5 font-[Poppins] rounded-md text-white md:w-auto w-full mt-4 ml-1">
+    Suscribirse
+</button>
+    </div>
+</div>
+        
+        
+          <div className="bg-pink-400 text-white flex flex-row justify-evenly items-center">
+            {/* <img src={logo} alt="logo" style={{
+                height: '10rem',
+                width: '10rem'
+            }} /> */}
+            <ItemsContainer />
+            <SocialIcons Icons={Icons} />
+            
           </div>
+        
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-center pt-2 text-white text-sm pb-8 bg-pink-400  ">
+          <span className="block text-white text-sm mx-auto">© 2024 PINKPANTHER. Todos los derechos reservados.</span>         
         </div>
-        <div>
-          <ItemsContainer/>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-center pt-2 text-gray-400 text-sm pb-8">
-          <span>© 2024 Appy. Todos los derechos reservados.</span>
-          <span>Términos · Política de privacidad</span>
-          <SocialIcons Icons={Icons}/>
-        </div>
+        
       </footer>
     );
   };
-  
-  
+
 export default Footer;
+
--- src/pages/Home/SingleProduct.jsx
+++ src/pages/Home/SingleProduct.jsx
@@ -2,87 +2,161 @@ import React, {useEffect,useState} from "react"
 import { FaArrowAltCircleRight, FaStar } from "react-icons/fa"
 import { useParams } from "react-router-dom"
 
-const SingleProduct =()=>{
-
-    const { id } = useParams();
-    const [product, setProduct] = useState({}); // Inicializar como un objeto vacío
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("products.json");
+const demoText = {
+    description:
+      'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+    highlights: [
+      "Hand cut and sewn locally",
+      "Dyed with our proprietary colors",
+      "Pre-washed & pre-shrunk",
+      "Ultra-soft 100% cotton",
+    ],
+    details:
+      'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+  };
+  
+  const SingleProduct = () => {
+      const {id} = useParams()
+      const [products, setProducts] = useState([]);
+      useEffect(() => {
+          // Scroll to the top when the component mounts
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+        }, []);
+      useEffect(() => {
+          const fetchData = async () => {
+              try {
+                const response = await fetch("/products.json");
                 const data = await response.json();
-                const product = data.find((p) => p.id == id);
-                if (product) {
-                    setProduct(product);
-                } else {
-                    console.error("Producto no encontrado para el ID:", id);
-                }
-            } catch (error) {
-                console.error("Error al obtener los datos:", error);
-            }
-        };
-
-        fetchData();
-    }, [id]);
-
-    const { title, category, price, image, status } = product;;
-
-
-    return(
-        <div className='mt-28 max-w-screen-2xl container mx-auto x1:px-28 px-4'>
-            <div className='p-3 max-w-7xl m-auto'>
-                <div className='mt-6 sm:mt-10'>
-
-                    <div className='text-black/75 mt-12'>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut qui, consequatur excepturi provident quaerat sed non perferendis reiciendis voluptatibus obcaecati aliquid repellat cupiditate vel dolor illo ad omnis facere sequi.
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quaerat beatae dolor natus, eligendi ex, hic a iste autem doloribus voluptate accusamus harum nulla soluta fuga, consectetur explicabo. Odit, fugiat!
-                        </p>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum a recusandae velit, culpa eaque cupiditate praesentium, adipisci similique animi reiciendis omnis unde quidem ducimus provident accusantium. Maxime alias totam eveniet.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci consectetur qui voluptatibus. Et facere voluptatum, incidunt sapiente magni quas quisquam veritatis deleniti accusantium provident explicabo, odio dolores vitae in assumenda?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel deleniti repudiandae nostrum ducimus eum dicta voluptatum inventore tempore, enim aut reiciendis, asperiores totam dolorum, iste perspiciatis! Fugit qui voluptatum pariatur!</p>
-                    </div>
-                    <div className='grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-6 h-max'>
-                        <img src={image} alt="" className='w-full'/>
-                    </div>
-
-                    <div>
-                        <h1 className='title text-left'>{title}</h1>
-                        <p className='mt-3 text-gray-600 text-base leading-6 text-justify sm:text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum hic ducimus modi veritatis maiores at id doloremque debitis corrupti natus tempore sit, quo tempora praesentium voluptatem officiis ullam ad excepturi.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iure nostrum tenetur atque! Doloribus eveniet sit eius! Veritatis, quod, facilis odit tempore eveniet ipsam mollitia iure similique quasi non eius.
-                        </p>
-
-                        <span className='my-2 text-xl text-yellow-400 flex items-center gap-1 sm:my-4'>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                            <FaStar/>
-                        </span>
-
-                        <p className='text-xl text-red-500 font-semibold sm:text-2xl'>${price}</p>
-                        <div className='mt-4'>
-                        <div className='text-left flex flex-col gap-2 w-full'>
-                            <label className='font-semibold'>Cantidad</label>
-                            <input type="number" name="price" id="price" defaulValue={1} required className="border border-gray-300 text-sm font-semibold mb-1 max-w-full w-full outline-none rounded-md m-0 py-3 px-4 md:py-3 md:px-4 focus:border-red-500 " />
-                        </div>    
-                        <div className='w-full text-left my-4'>
-                            <button className='flex justify-center items-center gap-2 w-full py-3 px-4 bg-red-500 text-white font-bold border-red-500 rounded-md ease-in-out duration-150 shadow-slate-600 hover:bg-white hover:text-red-500 lg:m-0 md:px-6 '>
-                                <span>
-                                    Confirmar orden</span>
-                                    <FaArrowAltCircleRight/> 
-                                    </button>
-                                    </div>   
-                   </div>
-                    </div>
-
-                   
-
-                </div>
-            </div>
+               const product = data.filter((p) => p.id == id)
+              //  console.log(product[0])
+               setProducts(product[0])
+              } catch (error) {
+                console.error("Error fetching data:", error);
+              }
+            };
+        
+            fetchData();
+      }, [id])
+  
+      const {image, title, category, price} = products;
+  
+  
+    return (
+      <div className="mt-28 max-w-screen-2xl container mx-auto xl:px-28 px-4">
+        
+        <div className="flex items-center gap-2 pt-8 text-Black/50">
+          <a href="/">Home</a> <a href="/shop" className="font-semibold text-black">/ Shop</a>
         </div>
-    )
-}
-
-export default SingleProduct
+  
+        <div className="p-3 max-w-7xl m-auto">
+          <div className="mt-6 sm:mt-10">
+            <div>
+            <div>
+              <div className="grid gird-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-6 h-max">
+                {/* Product Image */}
+                <div className="overflow-hidden rounded-xl">
+                  <img
+                    src={image}
+                    alt="Product-Image"
+                    className="w-full"
+                  />
+                </div>
+                {/* Product Details */}
+                <div className="flex flex-col justify-between">
+                  <div>
+                    {/* Product Title */}
+                    <h1 className="text-3xl text-red-500 font-semibold sm:text-4xl">
+                      {title}
+                    </h1>
+                    {/* Product Description */}
+                    <p className="mt-3 text-gray-600 text-md leading-6 text-justify sm:text-left sm:mt-4">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Sed enim ut sem viverra aliquet eget sit. Odio
+                      facilisis mauris sit amet
+                    </p>
+                    {/* Star Ratings */}
+                    <span className="my-3 text-xl text-yellow-600 flex items-center gap-1 sm:my-4">
+                      {Array.from({ length: 3 }).map((_, index) => (
+                        <FaStar key={index} />
+                      ))}
+                    </span>
+                    {/* Product Price */}
+                    <span className="text-xl text-red-500 font-semibold sm:text-2xl">
+                      ${price}
+                    </span>
+                  </div>
+                  {/* Cantidad Input and Order Button */}
+                  <div className=" ">
+                    <div className="text-left flex flex-col gap-2 w-full">
+                      {/* Cantidad Label */}
+                      <label className="font-semibold">Cantidad</label>
+                      {/* Cantidad Input */}
+                      <input
+                        className="border border-gray-300 text-sm font-semibold mb-1 max-w-full w-full outline-none rounded-md m-0 py-3 px-4 md:py-3 md:px-4 md:mb-0 focus:border-red-500"
+                        type="number"
+                        defaultValue="1"
+                        required
+                      />
+                    </div>
+                    {/* Order Button */}
+                    <div className="w-full text-left my-4">
+                      <button
+                        className="flex justify-center items-center gap-2 w-full py-3 px-4 bg-red-500 text-white text-md font-bold border border-red-500 rounded-md ease-in-out duration-150 shadow-slate-600 hover:bg-white hover:text-red-500 lg:m-0 md:px-6"
+                        title="Confirm Order"
+                      >
+                        <span>Confirmar Orden</span>
+                        <FaArrowAltCircleRight />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+  
+        {/* product details */}
+        <div className="mt-10">
+          <h2 className="text-sm font-medium text-gray-900">Detalles</h2>
+  
+          <div className="mt-4 space-y-6">
+            <p className="text-sm text-gray-600">
+              {demoText.details} {demoText.details}
+            </p>
+          </div>
+        </div>
+  
+        <div className="mt-10">
+          <h2 className="text-sm font-medium text-gray-900">Descricion</h2>
+  
+          <div className="mt-4 space-y-6">
+            <p className="text-sm text-gray-600">
+              {demoText.description} {demoText.description}
+            </p>
+          </div>
+        </div>
+  
+        <div className="mt-5">
+          <h2 className="text-sm font-medium text-gray-900">Destacados</h2>
+          <div className="mt-4 space-y-4">
+            <li className="text-sm text-gray-600">
+              {demoText.highlights[0]}
+            </li>
+            <li className="text-sm text-gray-600">
+              {demoText.highlights[1]}
+            </li>
+            <li className="text-sm text-gray-600">
+              {demoText.highlights[2]}
+            </li>
+            <li className="text-sm text-gray-600">
+              {demoText.highlights[3]}
+            </li>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default SingleProduct;
