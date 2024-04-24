21a7185bf08467e2552fb864a10acbdd64d40f39
Ivana Spinoso <138499741+ivanaspinoso@users.noreply.github.com>
2024-04-24 08:47:25 -0300

    Dev ivana (#12)
    * login y registro
    
    * actualizacion
    
    ---------
    
    Co-authored-by: ivana spinoso <ivanaspinoso@MacBook-Air-de-Agustin.local>

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Input = ({ handleChange, value, title, name }) => {
+    return (
+      <label className="sidebar-label-container">
+        <input onChange={handleChange} type="radio" value={value} name={name} />
+        <span className="checkmark" ></span>
+        {title}
+      </label>
+    );
+  };
+  
+  export default Input;
--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,12 @@ import {
 } from "react-router-dom";
 import Home from './pages/Home/Home.jsx';
 import SingleProduct from './pages/Home/SingleProduct';
-import AboutUs from "./components/AboutUs.jsx"
-
+import AboutUs from './components/AboutUs.jsx';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import Navbar from './components/Navbar.jsx';
+import Login from './components/Login.jsx';
+import CreateAccount from './components/CreateAccount.jsx';
 
 
 const router = createBrowserRouter([
@@ -26,10 +30,19 @@ const router = createBrowserRouter([
         element: <SingleProduct/>
       },
       {
-        path: "/about",
-        element: <AboutUs/>
-      }
-    ]
+
+        path: '/about',
+        element: <AboutUs />,
+      },
+      {path: '/login',
+      element: <Login />
+    },
+    {path: '/create-account',
+      element: <CreateAccount />
+    },
+      
+    ],
+
   },
 ]);
 
