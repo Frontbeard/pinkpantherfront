614c2cfe95c1919ea8a399a010673dc8a3132393
Issa Jaramillo <issakaterinejaramilloalvarez@gmail.com>
2024-04-24 14:14:41 -0500

    creacion de redux, reducer, action

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,22 +3,30 @@ import { Link } from "react-router-dom";
 
 const Card = ({ filteredItems }) => {
   return (
+    
     <div className="grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-center justify-center gap-12 shadow-sm">
-      {filteredItems.slice(0, 8).map((item) => (
-        <div key={item.id}>
+      {/* Mapeo de cada producto en la lista de productos filtrados */}
+      {filteredItems.map((item) => (
+        <div key={item.id}> 
+    
           <Link to={`/shop/${item.id}`}>
+           
             <img
-              src={item.image}
-              alt={item.title}
+              src={item.photo} 
+              alt={item.name} 
               className="mx-auto w-full hover:scale-105 transition-all duration-300"
             />
           </Link>
+         
           <div className="mt-4 px-4">
-            <h4 className="text-base font-semibold mb-2">{item.title}</h4>
-
+        
+            <h4 className="text-base font-semibold mb-2">{item.name}</h4>
+          
             <div className="flex justify-between">
+              
               <p className="text-black/50">{item.category}</p>
-              <p className="font-semibold">${item.price}</p>
+
+              <p className="font-semibold">${item.priceEfectivo}</p>
             </div>
           </div>
         </div>
