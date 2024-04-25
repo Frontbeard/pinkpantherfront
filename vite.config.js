90edb5cb2a3e414cbc4e69bc6980672b7f81c84f
ivana spinoso <ivanaspinoso@MacBook-Air-de-Agustin.local>
2024-04-25 08:33:30 -0300

    vite comentado

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      allow: ['C:/Users/Virginia/OneDrive/Documentos/Proyecto Final/pinkpantherfront']
-    }
-  }
+  
 });
