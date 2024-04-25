39359ef32921714ae8dc42f2071b1d62f36d49bc
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2024-04-24 21:49:12 -0300

    Cambios en funcionalidades de categrías de navBar y Filtrado por modal

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     fs: {
-      allow: ['C:/Users/Usuario/Desktop/pinkpantherfront']
+      allow: ['C:/Users/Virginia/OneDrive/Documentos/Proyecto Final/pinkpantherfront']
     }
   }
-})
+});
