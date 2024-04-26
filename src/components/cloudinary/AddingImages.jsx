00845c62d0ec2cec51bcb7483f7f9fc46f395e01
Florencia Accastelli <f.accastelli@gmail.com>
2024-04-26 11:39:07 -0300

    product image upload working

--- src/components/cloudinary/AddingImages.jsx
+++ src/components/cloudinary/AddingImages.jsx
@@ -4,15 +4,13 @@ const AddingImages = (props) => {
     const [image, setImage] = useState("");
     const [loading, setLoading] = useState(false);
 
-    const uploadImage = async (e) => {
-        console.log("este es el e")
-        console.log(e.target)
-        const files = e.target.files;
+    const uploadImage = async (event) => {
+        const files = event.target.files;
         const data = new FormData();
         data.append("file", files[0]);
         data.append("upload_preset", "images");
         setLoading(true);
-       /*  try { */
+        try {
             const res = await fetch(
                 "https://api.cloudinary.com/v1_1/dtjizedfr/image/upload",
                 {
@@ -21,23 +19,20 @@ const AddingImages = (props) => {
                 }
             );
             const file = await res.json();
-            console.log("uploadImage ejecutado"); 
-
             setImage(file.secure_url);
-       /*  } catch (error) {
-            console.error("Error uploading image: ", error); */
-       /*  } finally {
+        } catch (error) {
+            console.error("Error uploading image: ", error); 
+        } finally {
             setLoading(false);
-        } */
+        }
     };
 
     return (
         <div>
             <div style={{ textAlign: "left" }}>
-                <h1>Subiendo Imágenes</h1>
+                <h1>Subir imagen de producto</h1>
                 <form>
                 <input
-                    id="image123"
                     type="file"
                     name="imagen"
                     accept="image/*"
@@ -45,6 +40,7 @@ const AddingImages = (props) => {
                     style={{ marginBottom: "10px" }}
                 />
                 </form>
+                <img src={image} width="200px"/>
             </div>
         </div>
     );
