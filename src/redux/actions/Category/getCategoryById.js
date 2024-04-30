731ac17c82f84f228ddff107228c469862bcfef8
nicolasmray <140557114+nicolasmray@users.noreply.github.com>
2024-04-30 16:38:10 -0300

    accountlog (#70)
    Co-authored-by: nicolas.mray <nicolas.mray@gmail.com>

--- src/redux/actions/Category/getCategoryById.js
+++ src/redux/actions/Category/getCategoryById.js
@@ -1,14 +1,15 @@
 import { GET_CATEGORIES_BY_ID } from "../actions-types";
 import axios from "axios";
-
-const URL = 'https://pinkpanther-backend-ip0f.onrender.com/categories'
+import { URL_LINK } from '../../../URL.js'
+//const URL = 'https://pinkpanther-backend-ip0f.onrender.com/categories'
 
 
 
 export const getCategoryById = (categoryId) => {
     return async function (dispatch) {
         try {
-        let response = await axios.get(`${URL}/${categoryId}`)
+        let response = await axios.get(`${URL_LINK}/categories/${categoryId}`)
+        //let response = await axios.get(`${URL}/${categoryId}`)
         return dispatch({
             type: GET_CATEGORIES_BY_ID,
             payload: response.data
--- src/redux/actions/Favs/getFavoritesById.js
+++ src/redux/actions/Favs/getFavoritesById.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { GET_FAVORITES_BY_ID } from "../actions-types";
-
-const URL = 'https://pinkpanther-backend-ip0f.onrender.com/customer'
+import { URL_LINK } from '../../../URL.js'
+//const URL = 'https://pinkpanther-backend-ip0f.onrender.com/customer'
 
 export const getFavorites = (customerId)=>{
     return async(dispatch)=>{
         try {
-            const response = await axios.get(`${URL}/${customerId}/favorites`)
+            const response = await axios.get(`${URL_LINK}/customer/${customerId}/favorites`)
+            //const response = await axios.get(`${URL}/${customerId}/favorites`)
             if(!response.ok){
                 throw new Error ('Failed to fetch favorites')
             }
