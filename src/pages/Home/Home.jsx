e71842dd4decd150dd23f2c5f5395e8ad383825e
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2024-05-01 20:25:57 -0300

    cambio en el banner, aboutus sin filtermodal

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import Banner from './Banner'
-import Category from './Category'
-import Products from './Products'
-import Newsletter from '../NewsLetter'
+import React, { useState, useRef, useEffect } from 'react';
+import Banner from './Banner';
+import Category from './Category';
+import Products from './Products';
+import Newsletter from '../NewsLetter';
 
 const Home = () => {
+  const [showBanner, setShowBanner] = useState(true);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleVerAhoraClick = () => {
+    setShowBanner(false);
+  };
+
+  const handleBackToBannerClick = () => {
+    setShowBanner(true);
+    window.scrollTo(0, scrollPosition);
+  };
+
+  const handleScroll = () => {
+    setScrollPosition(window.pageYOffset);
+  };
+
   return (
     <div className='bg-gray-100'>
-      
-        <Banner/>
-        <Products/>
-        <Category/>
-        <Newsletter/>
-       
+      {showBanner ? (
+        <>
+          <Banner onVerAhoraClick={handleVerAhoraClick} />
+          <Products />
+        </>
+      ) : (
+        <Category onBackToBannerClick={handleBackToBannerClick} />
+      )}
+      <Newsletter />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
