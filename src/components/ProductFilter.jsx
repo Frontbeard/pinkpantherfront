029575f074e4363e6888ef9fe3954017e13e1556
FlorAccastelli <142617063+FlorAccastelli@users.noreply.github.com>
2024-05-01 14:28:41 -0300

    Revert "arreglando-errores"

--- src/components/ProductFilter.jsx
+++ src/components/ProductFilter.jsx
@@ -4,29 +4,30 @@ import Card from "./Card";
 
 const ProductFilter = ({ products }) => {
   const handleProductClick = (productId) => {
+    // Redirigir al usuario a la página de detalle del producto
     window.location.href = `/shop/${productId}`;
   };
 
-  if (products && products.length > 0) {
-    return (
-      <div className="max-w-screen-2xl container mx-auto xl:px-28 px-4 mb-12">
-        <div className="flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-6 mb-5">
-          <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap card-container">
-            {products.map((product) => (
-              <div key={product.id} onClick={() => handleProductClick(product.id)}>
-                <Link to={`/shop/${product.id}`}>
-                  <Card filteredItems={product} />
-                </Link>
-              </div>
-            ))}
-          </div>
+  return (
+    <div className="max-w-screen-2xl container mx-auto xl:px-28 px-4 mb-12">
+      <div className="flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-6 mb-5">
+        <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap card-container">
+          {products && products.map((product) => (
+            <div key={product.id} onClick={() => handleProductClick(product.id)}>
+              {/* Utiliza Link o NavLink para la navegación */}
+              <Link to={`/shop/${product.id}`}>
+                <Card filteredItems={product} />
+              </Link>
+            </div>
+          ))}
+
         </div>
       </div>
     );
   } else {
+    // Si no hay productos disponibles, no se renderiza nada
     return null;
   }
 };
 
 export default ProductFilter;
-
