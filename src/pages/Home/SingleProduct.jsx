585057bd6ccd696513bb36bab2efe8f8968f6304
Issa Jaramillo <issakaterinejaramilloalvarez@gmail.com>
2024-05-01 10:52:04 -0500

    ya se pueden ver los productos relacionados en el detalle

--- src/pages/Home/SingleProduct.jsx
+++ src/pages/Home/SingleProduct.jsx
@@ -2,38 +2,47 @@ import React, { useEffect, useState } from "react";
 import { FaArrowAltCircleRight, FaStar } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
-import { productbyID } from "../../redux/actions/Product/productById";
+import { productbyID} from "../../redux/actions/Product/productById";
+import Card from "../../components/Card"; // Importa tu componente de tarjeta
+import { getAllProducts} from "../../redux/actions/Product/getAllProducts";
 
-const demoText = {
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elited scelerisque turpis posuere. Mauris in felis id eros dapibus tristique. Sed vehicula vestibulum vehicula. Donec vestibulum purus non vestibulum fringilla.",
-  highlights: [
-    "Lorem ipsum dolor sit amet",
-    "Consectetur adipiscing elit",
-    "Sed do eiusmod tempor incididunt",
-    "Ut labore et dolore magna aliqua",
-  ],
-  details:
-    "Lorem ipsum dolor sitnec rutuismod, mauris sit amet rutrum tempor, odio lectus facilisis nisi, a scelerisque sem orci vel nunc. Fusce scelerisque eros a sem fermentum, ac convallis nisi dictum. Vivamus sit amet pretium eros.",
-};
 
 const SingleProduct = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const productDetails = useSelector(state => state.details);
-  console.log(productDetails);
+  const allProducts = useSelector(state => state.allproducts);
+
   const [loading, setLoading] = useState(true);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-    
+      
     // Fetch product details by ID
     dispatch(productbyID(id))
       .then(() => setLoading(false))
       .catch(() => setLoading(false));
   }, [dispatch, id]);
+  
+  useEffect(() => {
+    dispatch(getAllProducts())
+      .catch(error => console.log(error));
+  }, []); // Sin dependencias, se ejecutará una vez al montar el componente
+  
 
-  const { photo, name, size, priceEfectivo, priceCuotas } = productDetails;
+  const handleIncrement = () => {
+    setQuantity(prevQuantity => prevQuantity + 1);
+  };
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity(prevQuantity => prevQuantity - 1);
+    }
+  };
+
+  // Filtrar productos relacionados por categoría
+  const relatedProducts = allProducts.filter(product => product.idCategory === productDetails.idCategory && product.id !== productDetails.id);
 
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-28 px-4 bg-gray-100">
@@ -50,64 +59,61 @@ const SingleProduct = () => {
               <div>
                 <div>
                   <div className="grid gird-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-6 h-max">
-               
                     <div className="overflow-hidden rounded-xl">
                       <img
-                        src={photo}
+                        src={productDetails.photo}
                         alt="Product-Image"
                         className="w-full"
                       />
                     </div>
-                 
                     <div className="flex flex-col justify-between">
                       <div>
-                       
-
-
                         <h1 className="text-3xl text-black-500 font-semibold sm:text-4xl">
-                          {name}
+                          {productDetails.name}
                         </h1>
-                      
-
-                        <p className="mt-3 text-gray-600 text-md leading-6 text-justify sm:text-left sm:mt-4">
-                          {demoText.description}
-                        </p>
-                        
-
-
                         <span className="my-3 text-xl text-yellow-600 flex items-center gap-1 sm:my-4">
                           {Array.from({ length: 3 }).map((_, index) => (
                             <FaStar key={index} />
                           ))}
                         </span>
                         <span className="text-xl text-pink-500 font-semibold sm:text-2xl">
-                      Precio Efectivo: ${priceEfectivo}
-                     </span>
-                     <span className="text-xl text-pink-500 font-semibold sm:text-2xl">
-                  Precio Cuotas: ${priceCuotas}
-                  </span>
-
-                      </div>
-                     
-                      <div className=" ">
+                          Efectivo: ${productDetails.priceEfectivo}
+                        </span>
+                        <span className="text-xl text-pink-500 font-semibold sm:text-2xl">
+                          Cuotas: ${productDetails.priceCuotas}
+                        </span>
                         <div className="text-left flex flex-col gap-2 w-full">
-                         
-                          <label className="font-semibold">Tamaño</label>
-                       
+                          <label className="font-semibold">Talle</label>
                           <input
                             className="border border-gray-300 text-sm font-semibold mb-1 max-w-full w-full outline-none rounded-md m-0 py-3 px-4 md:py-3 md:px-4 md:mb-0 focus:border-red-500"
                             type="text"
-                            value={size}
+                            value={productDetails.size}
+                            readOnly
+                          />
+                          <label className="font-semibold">Color</label>
+                          <input
+                            className="border border-gray-300 text-sm font-semibold mb-1 max-w-full w-full outline-none rounded-md m-0 py-3 px-4 md:py-3 md:px-4 md:mb-0 focus:border-red-500"
+                            type="text"
+                            value={productDetails.color}
                             readOnly
                           />
                         </div>
-                        
-                        <div className="w-full text-left my-4">
+                        <div className="w-full text-left my-4 flex items-center justify-between">
+                          <div className="form-row m-0 align-items-center">
+                            <button className="text-2xl font-bold mr-2" onClick={handleDecrement}>-</button>
+                            <input
+                              type="text"
+                              className="w-16 text-center border border-gray-300 rounded-md"
+                              value={quantity}
+                              readOnly
+                            />
+                            <button className="text-2xl font-bold ml-2" onClick={handleIncrement}>+</button>
+                          </div>
                           <button
-                            className="flex justify-center items-center gap-2 w-full py-3 px-4 bg-pink-500 text-white text-md font-bold border rounded-md ease-in-out duration-150 shadow-slate-600 hover:bg-white hover:text-pink-500 lg:m-0 md:px-6"
-                            title="Confirm Order"
+                            className="flex justify-center items-center gap-2 py-3 px-4 bg-pink-500 text-white text-md font-bold border rounded-md ease-in-out duration-150 shadow-slate-600 hover:bg-white hover:text-pink-500 lg:m-0 md:px-6"
+                            title="Añadir al carrito"
                           >
-                            <span>Confirmar Orden</span>
+                            <span>Añadir al carrito</span>
                             <FaArrowAltCircleRight />
                           </button>
                         </div>
@@ -119,40 +125,12 @@ const SingleProduct = () => {
             </div>
           </div>
 
-          {/* product details */}
-          <div className="mt-8">
-            <h2 className="text-sm font-medium text-gray-900">Detalles</h2>
-            <div className="mt-4 space-y-6">
-              <p className="text-sm text-gray-600">
-                {demoText.details}
-              </p>
-            </div>
-          </div>
-
-          <div className="mt-4">
-            <h2 className="text-sm font-medium text-gray-900">Descripción</h2>
-            <div className="mt-4 space-y-6">
-              <p className="text-sm text-gray-600">
-                {demoText.description}
-              </p>
-            </div>
-          </div>
-
-          <div className="mt-4">
-            <h2 className="text-sm font-medium text-gray-900">Características</h2>
-            <div className="mt-4 space-y-4">
-              <li className="text-sm text-gray-600">
-                {demoText.highlights[0]}
-              </li>
-              <li className="text-sm text-gray-600">
-                {demoText.highlights[1]}
-              </li>
-              <li className="text-sm text-gray-600">
-                {demoText.highlights[2]}
-              </li>
-              <li className="text-sm text-gray-600">
-                {demoText.highlights[3]}
-              </li>
+          <div className="max-w-screen-2xl container mx-auto xl:px-28 px-4 bg-gray-100">
+            <h2 className="text-2xl font-bold mt-8 mb-4">Productos relacionados</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              {relatedProducts.map(product => (
+                <Card key={product.id} filteredItems={product} />
+              ))}
             </div>
           </div>
         </div>
