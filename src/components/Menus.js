f10f4dd88c632de28d6ac87993e6064a9d51c205
Tobias Dutra <109052055+TobiasDutra@users.noreply.github.com>
2024-05-02 13:28:15 -0300

    Correciones (#96)
    * Update Navbar.jsx
    
    links favorites y cart agregados a iconos
    
    * Update Menus.js
    
    cambiados links del footer para favorito y mi carrito

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,26 +1,26 @@
 export const PRODUCTS = [
-    { name: "Cuenta", link: "/login" },
-    { name: "Favorito", link: "#" },
-    { name: "Mi carrito", link: "#" },
-  ];
-  export const RESOURCES = [
-    { name: "Sobre nosotros", link: "/about" },
-    { name: "INGRESAR", link: "/create-account" },
-    { name: "Home", link: "/" },
-    
-  ];
-  export const COMPANY = [
-    { name: "Política de privacidad", link: "#" }
-  ];
-  
-  export const SUPPORT = [
-    { name: "Documentación", link: "#" },
-    { name: "Tutoriales y guías", link: "#" },
-    { name: "Webinars", link: "#" },
-    { name: "Código abierto", link: "#" },
-  ];
-  
-  export const Icons = [
-    { name: "logo-email", link: "Pink27854@gmail.com" },
-    { name: "logo-instagram", link: "https://www.instagram.com/pink.pantherindumentaria/" },
-  ];
+  { name: "Cuenta", link: "/login" },
+  { name: "Favorito", link: "/favorites" },
+  { name: "Mi carrito", link: "/cart" },
+];
+export const RESOURCES = [
+  { name: "Sobre nosotros", link: "/about" },
+  { name: "INGRESAR", link: "/create-account" },
+  { name: "Home", link: "/" },
+];
+export const COMPANY = [{ name: "Política de privacidad", link: "/privacy" }];
+
+export const SUPPORT = [
+  { name: "Documentación", link: "#" },
+  { name: "Tutoriales y guías", link: "#" },
+  { name: "Webinars", link: "#" },
+  { name: "Código abierto", link: "#" },
+];
+
+export const Icons = [
+  { name: "logo-email", link: "Pink27854@gmail.com" },
+  {
+    name: "logo-instagram",
+    link: "https://www.instagram.com/pink.pantherindumentaria/",
+  },
+];
