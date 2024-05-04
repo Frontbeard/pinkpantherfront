620e28fb2117eabaa5e19c611db3c2f293cf8501
nicolas.mray <nicolas.mray@gmail.com>
2024-05-04 16:23:52 -0300

    cart1

--- src/redux/actions/Cart/createCart.js
+++ src/redux/actions/Cart/createCart.js
@@ -1,19 +1,19 @@
 import axios from "axios";
-import { ADD_TO_CART } from "../actions-types";
+import { CREATE_CART } from "../actions-types.js";
 import { URL_LINK } from '../../../URL.js';
 
-export const addToCart = (customerId, productId, productQuantity, totalPrice, discounts) => {
+export const createCart = (customerId, productId, productQuantity) => {
+  console.log('Desde actions:', customerId, productId, productQuantity)
   return async (dispatch) => {
     try {
       const response = await axios.post(`${URL_LINK}/cart/create`, {
         productQuantity,
         customerId,
         productId,
-        totalPrice,
-        discounts,
       });
       dispatch({
-        type: ADD_TO_CART,
+        type: CREATE_CART,
+        payload: response
       });
     } catch (error) {
       console.error('Error al agregar el producto al carrito', error);
--- src/redux/actions/Cart/getCart.js
+++ src/redux/actions/Cart/getCart.js
@@ -3,19 +3,35 @@ import { GET_CART } from "../actions-types";
 import { URL_LINK } from '../../../URL.js'
 //const URL = 'https://pinkpanther-backend-ip0f.onrender.com/'
 
-export const getCart =(id)=>{
-    return async(dispatch)=>{
-        try {
-            const response = await axios.get(`${URL_LINK}/cart/${id}`)
-            //const response = await axios.get(`${URL}/cart/${id}`)
-            dispatch({
-                type:GET_CART,
-                payload: response.data.allCart
-            })
-        } catch (error) {
-            console.error('Error los productos del carrito', error);
-        }
+export const getCart =()=>{
+//     return async(dispatch)=>{
+//         try {
+//             const response = await axios.get(`${URL_LINK}/cart/${id}`)
+//             //const response = await axios.get(`${URL}/cart/${id}`)
+//             dispatch({
+//                 type:GET_CART,
+//                 payload: response.data.allCart
+//             })
+//         } catch (error) {
+//             console.error('Error los productos del carrito', error);
+//         }
+//     }
+// }
+
+return async function (dispatch) {
+    try {
+       let response = await axios.get(`${URL_LINK}/cart/`)
+       //let response = await axios.get(`${URL}`);
+       return dispatch({
+           type: GET_CART,
+           payload: response.data
+       })
+
+    } catch (error) {
+       console.error('Error al obtener todos los productos', error);
+       dispatch({ type: GET_CART, payload: error });
     }
+   }
 }
 
 export default getCart;
