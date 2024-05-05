d3d219a60a5f9df3b077471b0a407dee7a6b6197
Tobias Dutra <tobias.dutra2009@gmail.com>
2024-05-05 13:35:26 -0300

    cardOrder, createReview

--- src/components/CardOrder.jsx
+++ src/components/CardOrder.jsx
@@ -1,7 +1,16 @@
+import { Button, CardFooter } from "@material-tailwind/react";
 import React from "react";
 import { Link } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 
 const CardOrder = ({ filteredItems }) => {
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    navigate("/create-review");
+  }
     return (
         <div className="max-w-xs mx-auto">
           {filteredItems.products.map((product) => (
@@ -16,10 +25,15 @@ const CardOrder = ({ filteredItems }) => {
               <div className="mt-4 px-4">
                 <h4 className="text-base font-semibold mb-2">{product.name}</h4>
                 <div className="flex justify-between">
-                  <p className="text-black/50">{product.category}</p>
+                  <p className="text-black/50">{product.quantity}</p>
                   <p className="font-semibold">${product.priceEfectivo}</p>
                 </div>
               </div>
+              <CardFooter className="pt-0 mt-1">
+            <Button onClick={handleSubmit} className="text-white bg-pink-500" variant="gradient" fullWidth>
+              Calificar
+            </Button>
+          </CardFooter>
             </div>
           ))}
         </div>
