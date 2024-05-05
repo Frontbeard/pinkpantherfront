1fdf8558bac8a0602a68aac03e7473e1ee345e59
FlorAccastelli <142617063+FlorAccastelli@users.noreply.github.com>
2024-05-05 10:05:14 -0300

    Enable product (#138)
    * enable version 1. no funciona
    
    * enable product v1 for admin

--- src/redux/actions/Product/updateProduct.js
+++ src/redux/actions/Product/updateProduct.js
@@ -1,18 +1,14 @@
 import axios from "axios";
 import { UPDATE_PRODUCT } from "../actions-types";
 import { URL_LINK } from '../../../URL.js'
-//const URL = 'https://pinkpanther-backend-ip0f.onrender.com/product';
 
 export const updateProduct = (productId,newData)=>{
     return async (dispatch)=>{
       try {
         const response = await axios.put(`${URL_LINK}/product/${productId}`, newData)
-        //const response = await axios.put(`${URL}/${productId}`, newData)
-        const updatedProduct=response.data.product
-
         dispatch({
             type: UPDATE_PRODUCT,
-            payload: updatedProduct
+            payload: response.data.product
         })
         
       } catch (error) {
