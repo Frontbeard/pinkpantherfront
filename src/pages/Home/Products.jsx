c6600f674350f508b62b1720d9673319aeab76da
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2024-05-05 08:33:05 -0300

    filtrado por productos

--- src/pages/Home/Products.jsx
+++ src/pages/Home/Products.jsx
@@ -3,20 +3,32 @@ import { useDispatch, useSelector } from "react-redux";
 import Card from "../../components/Card";
 import Pagination from "../../components/Pagination";
 import { getAllProducts } from "../../redux/actions/Product/getAllProducts";
+import getAllCategories from "../../redux/actions/Category/getAllCategories";
+import filtBySize from "../../redux/actions/Filter/filtBySize";
+import filtByPrice from "../../redux/actions/Filter/filtByPrice";
+import orderProducts from "../../redux/actions/Filter/orderProducts";
 
 const Products = () => {
   const [filteredItems, setFilteredItems] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("all"); // Default: All
+  const [selectedCriterion, setSelectedCriterion] = useState(""); // Criterio seleccionado
+  const [criteriaOptions, setCriteriaOptions] = useState([]); // Opciones de filtro por producto
+  const [selectedSize, setSelectedSize] = useState("all"); // Default: All
+  const [priceRange, setPriceRange] = useState({ min: "", max: "" }); // Price range filter
   const [sortOption, setSortOption] = useState("default"); // Default sorting option
   const [currentPage, setCurrentPage] = useState(1); // Current page
   const [totalPages, setTotalPages] = useState(1); // Total pages
 
   const dispatch = useDispatch();
   const products = useSelector((state) => state.allproducts);
-  console.log(products);
+  const categories = useSelector((state) => state.allcategories); // Obtener las categorías desde el estado
 
   useEffect(() => {
     dispatch(getAllProducts());
+    dispatch(getAllCategories()); // Obtener todas las categorías al cargar el componente
+
+    // Obtener las opciones únicas de criterio de los productos
+    const uniqueCriteriaOptions = [...new Set(products.map(product => product.name))];
+    setCriteriaOptions(uniqueCriteriaOptions);
   }, [dispatch]);
 
   useEffect(() => {
@@ -30,54 +42,74 @@ const Products = () => {
     setCurrentPage(page);
   };
 
-  const itemsPerPage = 8;
+  const itemsPerPage = 4;
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const paginatedItems = filteredItems.slice(startIndex, endIndex);
 
- 
   if (!Array.isArray(paginatedItems)) {
-    return null; // o maneja el error de manera adecuada
+    return null;
   }
 
-  const filterItems = (category) => {
-    const filtered =
-      category === "all"
-        ? products
-        : products.filter((item) => item.category === category);
+  const applyFilters = () => {
+    let filteredProducts = [...products];
 
-    setFilteredItems(filtered);
-    setSelectedCategory(category);
+    // Filtrar por nombre del producto
+    if (selectedCriterion !== "") {
+      filteredProducts = filteredProducts.filter(product =>
+        product.name.toLowerCase().includes(selectedCriterion.toLowerCase())
+      );
+    }
+
+    // Filtrar por tamaño
+    if (selectedSize !== "all") {
+      filteredProducts = filteredProducts.filter(product => product.size === selectedSize);
+    }
+
+    // Filtrar por rango de precio
+    if (priceRange.min !== "" && priceRange.max !== "") {
+      filteredProducts = filteredProducts.filter(product => product.priceEfectivo >= parseInt(priceRange.min) && product.priceEfectivo <= parseInt(priceRange.max));
+    }
+
+    setFilteredItems(filteredProducts);
     setCurrentPage(1);
-    setTotalPages(Math.ceil(filtered.length / itemsPerPage));
+    setTotalPages(Math.ceil(filteredProducts.length / itemsPerPage));
   };
 
-  const showAll = () => {
+  const clearFilters = () => {
+    setSelectedCriterion("");
+    setSelectedSize("all");
+    setPriceRange({ min: "", max: "" });
+    setSortOption("default"); // Establecer la opción de ordenamiento de nuevo a "Por defecto"
     setFilteredItems(products);
-    setSelectedCategory("all");
     setCurrentPage(1);
     setTotalPages(Math.ceil(products.length / itemsPerPage));
   };
 
   const handleSortChange = (option) => {
+    setSelectedCriterion("");
+    setSelectedSize("all");
+    setPriceRange({ min: "", max: "" });
+
     setSortOption(option);
 
-    let sortedItems = [...filteredItems];
+    let sortedItems = [...products];
 
     switch (option) {
       case "A-Z":
-        sortedItems.sort((a, b) => a.title.localeCompare(b.title));
+        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
         break;
       case "Z-A":
-        sortedItems.sort((a, b) => b.title.localeCompare(a.title));
+        sortedItems.sort((a, b) => b.name.localeCompare(a.name));
         break;
-      case "low-to-high":
-        sortedItems.sort((a, b) => a.price - b.price);
+      case "Lowest Price":
+        sortedItems.sort((a, b) => a.priceEfectivo - b.priceEfectivo);
         break;
-      case "high-to-low":
-        sortedItems.sort((a, b) => b.price - a.price);
+      case "Highest Price":
+        sortedItems.sort((a, b) => b.priceEfectivo - a.priceEfectivo);
         break;
       default:
+        // No hacer nada para el caso de "default"
         break;
     }
 
@@ -89,8 +121,77 @@ const Products = () => {
       <h2 className="text-3xl font-semibold capitalize text-center my-8">
         Nuestros productos
       </h2>
+      <div className="flex justify-between items-center mb-4">
+        {/* Aquí van los filtros de categoría, tamaño, precio */}
+        <div>
+          <label htmlFor="criterionFilter">Producto:</label>
+          <input
+            type="text"
+            id="criterionFilter"
+            placeholder="Ingrese el producto"
+            value={selectedCriterion}
+            onChange={(e) => setSelectedCriterion(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="sizeFilter">Tamaño:</label>
+          <select
+            id="sizeFilter"
+            value={selectedSize}
+            onChange={(e) => setSelectedSize(e.target.value)}
+          >
+            <option value="all">Todos</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="unico">unico</option>
+            {/* Agrega más opciones según tus tamaños disponibles */}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="minPrice">Precio Mínimo:</label>
+          <input
+            type="number"
+            id="minPrice"
+            value={priceRange.min}
+            onChange={(e) =>
+              setPriceRange({ ...priceRange, min: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="maxPrice">Precio Máximo:</label>
+          <input
+            type="number"
+            id="maxPrice"
+            value={priceRange.max}
+            onChange={(e) =>
+              setPriceRange({ ...priceRange, max: e.target.value })
+            }
+          />
+        </div>
+        <button onClick={applyFilters}>Filtrar</button>
+        <button onClick={clearFilters}>Limpiar</button>
+      </div>
+      <div className="flex justify-between items-center mb-4">
+        <div>
+          <label htmlFor="sortOptions">Ordenar por:</label>
+          <select
+            id="sortOptions"
+            value={sortOption}
+            onChange={(e) => handleSortChange(e.target.value)}
+          >
+            <option value="default">Por defecto</option>
+            <option value="A-Z">A-Z</option>
+            <option value="Z-A">Z-A</option>
+            <option value="Lowest Price">Menor Precio</option>
+            <option value="Highest Price">Mayor Precio</option>
+          </select>
+        </div>
+      </div>
       <div className="flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-3 mb-8">
-        {/* Aquí renderizamos los productos paginados */}
         {paginatedItems.map((product) => (
           <Card key={product.id} filteredItems={product} />
         ))}
@@ -105,4 +206,3 @@ const Products = () => {
 };
 
 export default Products;
-
