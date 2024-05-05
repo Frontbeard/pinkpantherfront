bdd3075643d0a1c72c48c77125b6dbe32e225d44
Frontbeard <lufbenitez@gmail.com>
2024-05-05 20:20:24 -0300

    cambie los estilos de Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,47 @@ import { Link } from "react-router-dom";
 
 const Card = ({ filteredItems }) => {
   return (
-    <div className="max-w-xs mx-auto">
+    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-pink-100 dark:border-pink-100">
       <Link to={`/shop/${filteredItems.id}`}>
         <img
-          src={filteredItems.photo} 
-          alt={filteredItems.name} 
-          className="w-full h-64 object-cover hover:scale-105 transition-all duration-300"
+          src={filteredItems.photo}
+          alt={filteredItems.name}
+          className="p-8 rounded-t-lg"
         />
       </Link>
-      <div className="mt-4 px-4">
-        <h4 className="text-base font-semibold mb-2">{filteredItems.name}</h4>
-        <div className="flex justify-between">
-          <p className="text-black/50">{filteredItems.category}</p>
-          <p className="font-semibold">${filteredItems.priceEfectivo}</p>
+      <div className="px-5 pb-5">
+        <Link to={`/shop/${filteredItems.id}`}>
+          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-black">{filteredItems.name}</h5>
+        </Link>
+        <div className="flex items-center mt-2.5 mb-5">
+          <div className="flex items-center space-x-1 rtl:space-x-reverse">
+            {[...Array(4)].map((_, index) => (
+              <svg
+                key={index}
+                className="w-4 h-4 text-yellow-300"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 22 20"
+              >
+                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+              </svg>
+            ))}
+            <svg
+              className="w-4 h-4 text-black dark:text-black"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+          </div>
+          <span className="bg-pink-100 text-pi text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-pink-200 dark:text-black ms-3">5.0</span>
+        </div>
+        <div className="flex items-center justify-between">
+          <span className="text-3xl font-bold text-black dark:text-black">${filteredItems.priceEfectivo}</span>
+          <a href="#" className="text-white bg-pink-400 hover:bg-pink-400 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-pink-400 dark:hover:bg-pink-300 dark:focus:ring-pink-400">Agregar al carrito</a>
         </div>
       </div>
     </div>
