a21019faf4354a029c48b09ae66c77acbfeb3753
Frontbeard <lufbenitez@gmail.com>
2024-05-05 21:45:49 -0300

    carousel auto

--- src/pages/Home/Carousel.jsx
+++ src/pages/Home/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Carousel = () => {
   const [currentItem, setCurrentItem] = useState(0);
@@ -7,6 +7,16 @@ const Carousel = () => {
     { image: "/carousel2.png", link: "/page2" },
     { image: "/carousel1.png", link: "/page3" },
   ];
+  const intervalTime = 5000; 
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+
+      setCurrentItem((prev) => (prev === items.length - 1 ? 0 : prev + 1));
+    }, intervalTime);
+
+    return () => clearInterval(interval);
+  }, [currentItem]); 
 
   const prevItem = () => {
     setCurrentItem((prev) => (prev === 0 ? items.length - 1 : prev - 1));
@@ -36,39 +46,16 @@ const Carousel = () => {
         className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         onClick={prevItem}
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
-          </svg>
-          <span className="sr-only">Previa</span>
-        </span>
       </button>
       <button
         type="button"
         className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         onClick={nextItem}
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-          </svg>
-          <span className="sr-only">Siguiente</span>
-        </span>
       </button>
     </div>
   );
 };
 
 export default Carousel;
+
