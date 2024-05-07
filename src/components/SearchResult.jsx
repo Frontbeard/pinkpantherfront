d09d712d7676678b8d0c3dc4e26aee5c6193680d
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2024-05-07 08:47:38 -0300

    Debuggeando (#175)
    * agregué paginado a Searchbar
    
    * cambios en filtermodal, searchbar y productos

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
 import Card from './Card';
+import Pagination from './Pagination'; // Importar tu componente Pagination
 
 const SearchResult = () => {
   const { query } = useParams();
   const searchedProducts = useSelector(state => state.allproducts);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 2; // Número de productos por página
 
-  // Verificar si searchedProducts es un array antes de aplicar filter
-  const filteredProducts = Array.isArray(searchedProducts) ? searchedProducts.filter(product =>
-    product.name.toLowerCase().includes(query.toLowerCase())
-  ) : [];
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = searchedProducts.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const totalPages = Math.ceil(searchedProducts.length / productsPerPage);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-28 px-4 mb-12">
       <div className="search-result-container">
         <h2>Resultados de búsqueda para "{query}"</h2>
-        <div className="product-list grid grid-cols-4 gap-4"> {/* Usamos grid con 4 columnas y un espacio entre ellas */}
-          {filteredProducts.length > 0 ? (
-            filteredProducts.map(product => (
+        <div className="product-list grid grid-cols-4 gap-4">
+          {currentProducts.length > 0 ? (
+            currentProducts.map(product => (
               <Card key={product.name} filteredItems={product} />
             ))
           ) : (
             <div className="not-found-message">No se encontraron productos</div>
           )}
         </div>
+        {totalPages > 1 && (
+          <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+        )}
       </div>
       <Link to="/" className="btn btn-primary">Volver a la página de inicio</Link>
     </div>
@@ -32,6 +43,7 @@ const SearchResult = () => {
 };
 
 export default SearchResult;
+
 
 
 
