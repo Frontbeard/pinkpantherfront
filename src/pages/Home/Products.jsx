3ebae27631f99c85f3ca75945f2c20a39879ee43
Frontbeard <lufbenitez@gmail.com>
2024-05-07 20:06:54 -0300

    cambié botón de carrito en cards

--- src/pages/Home/Products.jsx
+++ src/pages/Home/Products.jsx
@@ -125,10 +125,9 @@ const Products = () => {
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-28 px-4 mb-12">
       <h2 className="text-3xl font-semibold capitalize text-center my-8">
-        Nuestros productos
+        NUESTROS PRODUCTOS
       </h2>
       <div className="flex justify-between items-center mb-4">
-        {/* Aquí van los filtros de categoría, tamaño, precio */}
         <div>
   <label htmlFor="criterionFilter">Producto:</label>
   <input
